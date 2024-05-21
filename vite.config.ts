44fc20cd2096d2d4570392b6fc3429d720cd275f
Celso Blackfyre <celsomartinscarvalho@gmail.com>
2024-05-21 08:29:03 -0300

    repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
+base: "/https://github.com/CelsoBlackfyre/atividade2/";
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+});
